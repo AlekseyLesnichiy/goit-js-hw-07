9cccb4468320c7d80a7ec4b7ce7b6571eccbe818
AlekseyLesnichiy <91471392+AlekseyLesnichiy@users.noreply.github.com>
2022-01-19 00:37:59 +0200

    s

---
+++ js/01-gallery.js
@@ -0,0 +1,73 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const createdGallery = makeGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", createdGallery);
+
+
+function makeGallery(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+             <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+            </a>
+        </div> `}).join(" ");
+   
+}
+
+
+
+gallery.addEventListener('click', onImgClick);
+const instance = basicLightbox.create(`<img class="modal-img" src="">`, {
+  onShow: (instance) => {
+    window.addEventListener('keydown', onEscClick);
+  },
+
+  onClose: (instance) => {
+    window.removeEventListener('keydown', onEscClick);
+  },
+});
+
+/*
+ * Function that gets executed before the lightbox will be shown.
+	 * Returning false will prevent the lightbox from showing.
+	 */
+	// onShow: (instance) => {},
+	/*
+	 * Function that gets executed before the lightbox closes.
+	 * Returning false will prevent the lightbox from closing.
+	 */
+	// onClose: (instance) => {}
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance.element().querySelector('img').src = evt.target.dataset.source;
+
+  instance.show();
+}
+
+// .element()
+// Returns the DOM element/node associated with the instance.
+
+// Example:
+
+// const elem = instance.element()
+
+function onEscClick(evt) {
+  if (evt.key === 'Escape') {
+    instance.close();
+    return;
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const createdGallery = makeGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", createdGallery);
+
+function makeGallery(obj) {
+    return obj.map(({ preview, original, description }) => {
+        return `
+       <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}"
+                alt="${description}"
+                title ="${description}" />
+        </a>`
+  
+    }).join(" ");
+}
+gallery.addEventListener("click", onClick);
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+});
+// const img = document.querySelector("img");
+// console.log(img.getAttribute("alt"));
+function onClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+   
+}
